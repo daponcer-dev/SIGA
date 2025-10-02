ca2956b1ff4cadc9748f112f571ff67e97d5eff3
daponcer-dev <daponcer.dev@gmail.com>
2025-10-01 20:43:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 
+function App() {
+  const [mensaje, setMensaje] = React.useState("");
+
+  function llamarAppsScript() {
+    google.script.run
+      .withSuccessHandler((res) => setMensaje(res))
+      .miFuncionDeAppsScript(); // función definida en tu Code.gs
+  }
+
+  return (
+    <div>
+      <h1>React + Apps Script + Blogger</h1>
+      <button onClick={llamarAppsScript}>Llamar Apps Script</button>
+      <p>Respuesta: {mensaje}</p>
+    </div>
+  );
+}
+
+export default App;
